9eff9797daf08416ad1095399ea36482ab6c298e
leangsengLy <edeepleang@gmail.com>
2025-02-11 23:21:47 +0700

    add

--- hello.java
+++ hello.java
@@ -1,17 +1,24 @@
 class hello{
     public static void main(String a[]){
-        int num1 = 20;
-        int num2 = 30;
-        byte by = 127;
-        short sh = 556;
+        // int num1 = 20;
+        // int num2 = 30;
+        // byte by = 127;
+        // short sh = 556;
 
-        long l =12313l;
-        float f = 6.5f;
-        double d = 5.4;
-        char c = 'k';
-        boolean b = true;
-        int result= num1 + num2;
-        System.out.println("Hello world Mr leangseng");
-        System.out.println(result);
+        // long l =12313l;
+        // float f = 6.5f;
+        // double d = 5.4;
+        // char c = 'k';
+        // boolean b = true;
+        // int result= num1 + num2;
+        // System.out.println("Hello world Mr leangseng");
+        // System.out.println(result);
+
+        int i=0;
+        while(i<=10){
+            System.out.println("Hello world "+i);
+            i++;
+        }
+        System.out.println("Hi");
     }
 }
